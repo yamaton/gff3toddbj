b7840d4f32469a7ce4b518852ae34de7e17cbb9b
Yamato Matsuoka <yamaton@gmail.com>
2021-08-29 13:28:03 -0400

    Update translators

--- src/modules/translators.py
+++ src/modules/translators.py
@@ -1,5 +1,7 @@
-from typing import Any, Dict, List, Iterable
+from typing import Any, Dict, List, Tuple, Iterable
 import json
+import re
+from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature
 
@@ -11,29 +13,63 @@ def load_as_dict(filepath) -> Dict[str, Any]:
         d = json.load(fp)
     return d
 
+
 def merge_dicts(dict_args: Iterable[Dict]):
     """
     Given any number of dictionaries, shallow copy and merge into a new dict,
     precedence goes to key-value pairs in latter dictionaries.
+
+    >>> merge_dicts([{'us', 1, 'canada, 1}, {'egypt': 20}, {'greece': 30, 'netherlands', 31}])
+    {'us', 1, 'canada, 1, 'egypt': 20, 'greece': 30, 'netherlands', 31}
     """
     res = {}
     for d in dict_args:
         res.update(d)
     return res
 
-class GFF3AttributesToQualifiers(object):
-    """Convert GFF3 attributes into qualifiers according to definition in JSON files.
 
-    Paths to JSON files are required at the instantiation.
-    JSON must have the schema
+def get_assembly_gap(seq: Seq) -> List[Tuple[int, int]]:
+    """
+    Get assembly_gap locations from seq.
+
+    [NOTE] A location is of format (begin, end)
+    with 1-based indexing, and inclusive in both sides.
+
+    >>> s = Seq("atatnnngattacanccc")
+    [(5, 7), (15, 15)]
+    """
+    s = str(seq)
+    patt = re.compile("n+")
+    matches = patt.finditer(s)
+
+    segments = []
+    for m in matches:
+        a, b = m.span()  # 0-based, left-inclusive, right-exclusive
+        tup = (a + 1, b) # 1-based, both-inclusive
+        segments.append(tup)
+
+    return segments
+
+
+class TranslateQualifiers(object):
+    """Translate annotation qualifiers according to table given in JSON.
+
+    Meant for renaming GFF3 attributes to DDBJ annotation qualifier keys.
+    JSON should have the form like following.
 
     ```
-    <feature-name-as-in-type>: {
-        "target": <corrected-feature-name>,
-        "prefix": <prefix>,  (optional)
+    {
+        "ID": {
+            "target": "note",
+            "prefix": "ID:",
+        },
+        "Name": {
+            "target": "",
+        }
     }
     ```
 
+    Empty "target" value means the name key is dropped.
     """
     def __init__(self, filepaths: Iterable[str]):
         self.paths = filepaths
@@ -71,14 +107,25 @@ class GFF3AttributesToQualifiers(object):
         return res
 
 
-class GFF3TypesToFeatures(object):
-    """Convert GFF3 types into features according to definition in JSON files
+class TranslateFeatures(object):
+    """Translate annotation features according to table given in JSON.
+
+    Meant for renaming GFF3 types to DDBJ annotation feature keys.
+    JSON should have the form like following.
 
     ```
-    <name-as-in-type>: {
-        "target": <corrected-feature-name>,
+    {
+        "five_prime_UTR": {
+            "target": "5'UTR"
+        },
+        "three_prime_UTR": {
+            "target": "3'UTR"
+        }
     }
     ```
+
+    Empty "target" value means the name key is dropped.
+    Unlike qualifiers, "prefix" is not supported here.
     """
     def __init__(self, filepaths: Iterable[str]):
         self.paths = filepaths
@@ -100,3 +147,4 @@ class GFF3TypesToFeatures(object):
                 new_name = self.d[name]["target"]
                 feature.type = new_name
         return features
+
