a86cc3241dc3a90f6e3c965d92a6767abdc77825
Yamato Matsuoka <yamaton@gmail.com>
2021-09-30 18:42:38 -0400

    Add a golden test for FASTA-only case (no GFF3)

--- tests/README.md
+++ tests/README.md
@@ -21,3 +21,4 @@ $ pytest --doctest-modules ..
 
 * RefSeq: [GCF_000280675.1](https://www.ncbi.nlm.nih.gov/assembly/GCF_000280675.1/)
 * EMBLmyGFF3: [FASTA and GFF3 data](GCF_000280675.1) from augustus, maker, and prokka
+* Spinach sequence: [DDBJ news 2021-08-10](https://www.ddbj.nig.ac.jp/news/en/2021-08-10_3-e.html)
--- tests/golden/test_golden.py
+++ tests/golden/test_golden.py
@@ -6,7 +6,7 @@ import pathlib
 import uuid
 
 WORK_DIR = pathlib.Path(__file__).parent
-TEST_OUTPUT_DEFAULT_PREFIX = "testout_"
+TEST_OUTPUT_DEFAULT_PREFIX = "pytest_"
 
 
 # [TODO] Rewrite with dataclass when support for Python 3.6 is gone.
@@ -20,30 +20,27 @@ class BunchOfFiles(object):
         self.metadata = input_metadata
         self.transl_table = transl_table
 
+    def get_command(self, output: Union[str, pathlib.Path]) -> str:
+        options = {
+            "gff3": self.gff3,
+            "fasta": self.fasta,
+            "metadata": self.metadata,
+            "transl_table": self.transl_table,
+            "out": output,
+        }
+        options = {k: v for k, v in options.items() if v is not None}
+        s = " ".join("--{}={}".format(k, v) for k, v in options.items())
+        command = "gff3-to-ddbj {} 2> /dev/null".format(s)
+        return command
 
-def get_command(data: BunchOfFiles, output: Union[str, pathlib.Path]):
-    command = """\
-    gff3-to-ddbj \
-        --gff3={gff3} \
-        --fasta={fasta} \
-        --transl_table={transl_table} \
-        --metadata={metadata} \
-        --output={output} \
-        2> stderr.log
-    """.format(
-        gff3=data.gff3,
-        fasta=data.fasta,
-        metadata=data.metadata,
-        transl_table=data.transl_table,
-        output=output,
-    )
-    return command
+    def get_ref(self):
+        return self.ref
 
 
 def runner(data: BunchOfFiles, prefix:str=""):
     filename = "_".join([TEST_OUTPUT_DEFAULT_PREFIX, prefix, str(uuid.uuid1()) + ".ann"])
     output_file = WORK_DIR / filename
-    cmd = get_command(data, output_file)
+    cmd = data.get_command(output_file)
     subprocess.run(cmd, shell=True)
 
     with open(data.ref, "r") as f_ref, open(output_file, "r") as f_out:
@@ -97,3 +94,13 @@ def tests_prokka():
         11,  # transl_table
     )
     runner(testdata_prokka, "prokka")
+
+
+def tests_without_gff3():
+    testdata_no_gff3 = BunchOfFiles(
+        WORK_DIR / "fasta_only.ann",
+        None,  # no GFF3
+        WORK_DIR / "fasta_only.fna.gz",
+        WORK_DIR / "metadata_without_COMMON.toml",
+    )
+    runner(testdata_no_gff3, "no-gff3")
